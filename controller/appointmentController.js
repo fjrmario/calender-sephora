7b3671718d8f0f8233b7693a8587d583070e3431
Harold <haroldquekhere@gmail.com>
2023-03-31 17:51:57 +0800

    fix the appt route and controller

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -3,16 +3,24 @@ const Appointment = require("../model/appointmentModel");
 const create = async (req, res) => {
     try {
       const createAppointment = await Appointment.create(req.body);
-      res.status(200).json(createAppointment);
+      console.log(req.body);
+      const newAppointment = await createAppointment.save();
+      console.log(newAppointment);
+      res.status(200).json(newAppointment);
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
   };
 
   const deleteAppointment = async (req, res) => {
+    const {id} = req.params
     try {
-      const deleteAppt = await Appointment.findByIdAndRemove(req.params.id);
-      res.status(200).json(deleteAppt);
+      const findAppt = await Appointment.findOne({_id: id });
+      if (!findAppt) {
+        return res.status(404).json({ error: 'Appointment not found' });
+      }
+      const deleteAppointment = await Appointment.deleteOne({_id: findAppt._id})
+      res.status(200).json(deleteAppointment);
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -2,7 +2,7 @@ const appointmentController = require("../controller/appointmentController");
 const express = require("express");
 const router = express.Router();
 
-router.delete("/:id", appointmentController.deleteAppointment);
-router.post("/booking", appointmentController.create)
+router.delete("/:_id", appointmentController.deleteAppointment);
+router.post("/", appointmentController.create)
 
 module.exports = router;
