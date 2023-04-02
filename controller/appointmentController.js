5ec6c0ff6250e46facfc58394922fdbfcf8570b6
fjrmario <114366692+fjrmario@users.noreply.github.com>
2023-04-02 16:24:03 +0800

    fix create appt

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -1,53 +1,17 @@
 const Appointment = require("../model/appointmentModel");
-const MakeupArtist = require("../model/makeupArtistModel")
+// const MakeupArtist = require("../model/makeupArtistModel");
 
-  const create = async (req, res) => {
-    const { name, email, location, artist, date, timeslot } = req.body;
-  
-    try {
-      // Find the MakeupArtist by the selected artist name
-      const makeupArtist = await MakeupArtist.findOne({ name: artist });
-  
-      if (!makeupArtist) {
-        return res.status(404).json({ error: "Makeup artist not found" });
-      }
-  
-      // Check if the email already exists in the timeslot array for the selected timeslot
-      const selectedTimeslot = makeupArtist.timeslot.find(
-        (ts) => ts.time === timeslot && ts.date === date
-      );
-      if (selectedTimeslot && selectedTimeslot.bookings.includes(email)) {
-        return res
-          .status(400)
-          .json({ error: "Email already booked for this timeslot" });
-      }
-  
-      const createAppointment = await Appointment.create({
-        customerName: name,
-        location: location,
-        makeupArtist: artist,
-        customerInfo: {
-          name: name,
-          email: email,
-        },
-        date: date,
-        timeslot: timeslot,
-      });
-  
-      // Push the email to the timeslot array of the MakeupArtist for the selected timeslot
-      if (selectedTimeslot) {
-        selectedTimeslot.bookings.push(email);
-      } else {
-        makeupArtist.timeslot.push({ time: timeslot, date: date, bookings: [email] });
-      }
-  
-      await makeupArtist.save();
-  
-      res.status(200).json(createAppointment);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
+const create = async (req, res) => {
+  try {
+    const createAppointment = await Appointment.create(req.body);
+    console.log(req.body);
+    const newAppointment = await createAppointment.save();
+    console.log(newAppointment);
+    res.status(200).json(newAppointment);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
   const deleteAppointment = async (req, res) => {
     const {id} = req.params
