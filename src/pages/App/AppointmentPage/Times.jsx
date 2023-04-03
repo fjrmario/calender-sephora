f228f4772fe7c8a2a65a8336bd044baecf61fba9
Harold <haroldquekhere@gmail.com>
2023-04-03 10:07:02 +0800

    fix calendar.jsx

--- src/pages/App/AppointmentPage/Times.jsx
+++ src/pages/App/AppointmentPage/Times.jsx
@@ -48,7 +48,7 @@ function Times({ date, selectArtist, customerInfo }) {
       console.error("Error creating appointment:", error);
     }
   };
-  console.log("selectArtist:", JSON.stringify(selectArtist, null, 2));
+  console.log("selectArtist:", JSON.stringify(selectArtist));
 
   const displayInfo = async (e) => {
     setInfo(true);
@@ -76,7 +76,9 @@ function Times({ date, selectArtist, customerInfo }) {
   };
 
     const todayDate = new Date();
+    todayDate.setHours(0, 0, 0, 0);
     const futureDate = date > todayDate;
+    console.log(`date in Times: ${date}`)
 
     useEffect(() => {
       setDateChanged(true);
@@ -179,7 +181,7 @@ function Times({ date, selectArtist, customerInfo }) {
           const endTime = times.split(" - ")[1];
           //console.log(startTime)
           const disabledTime =
-            timeSlotDisabled(startTime, futureDate, endTime, selectArtist)
+            timeSlotDisabled(startTime, futureDate, endTime, selectArtist);
             return !disabledTime ? (
               <div key={index}>
                 <button onClick={(e) => displayInfo(e)}>{times}</button>
