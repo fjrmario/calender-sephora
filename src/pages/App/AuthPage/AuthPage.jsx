5de5c0af01367f3f8963a374c4c5e5822dbc2701
Harold <haroldquekhere@gmail.com>
2023-04-07 09:55:02 +0800

    insert useLocation into AuthPage

--- src/pages/App/AuthPage/AuthPage.jsx
+++ src/pages/App/AuthPage/AuthPage.jsx
@@ -1,25 +1,28 @@
-import {Routes, Route, Link} from "react-router-dom"
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import SignUpForm from "./SignUpForm";
 import LoginForm from "./LoginForm";
 import ForgetPassword from "./ForgetPassword";
 
 export default function AuthPage({ setUser }) {
+  const location = useLocation();
 
   return (
     <>
-    <Link to="/signup">
-        <button>New Customer</button>
-    </Link>
-    <Link to="/login">
+      {location.pathname !== "/forgetpassword" && (
+        <Link to="/signup">
+          <button>New Customer</button>
+        </Link>
+      )}
+      <Link to="/login">
         <button>Log in</button>
-    </Link>
-    <Link to="/forgetpassword">
+      </Link>
+      <Link to="/forgetpassword">
         <button>Forget Password</button>
-    </Link>
+      </Link>
       <Routes>
-        <Route path="/login" element={<LoginForm setUser={setUser} />}/>
-        <Route path="/signup" element={<SignUpForm/>}/>
-        <Route path="/forgetpassword" element={<ForgetPassword/>}/>
+        <Route path="/login" element={<LoginForm setUser={setUser} />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
       </Routes>
     </>
   );
