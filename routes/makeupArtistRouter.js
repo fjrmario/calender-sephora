fdba178008a4f20a7b0dcd6020300e65568993e5
Harold <haroldquekhere@gmail.com>
2023-04-08 16:18:40 +0800

    edit NavBar.jsx

--- routes/makeupArtistRouter.js
+++ routes/makeupArtistRouter.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const makeupArtist = require("../controller/makeupAdminController");
 const {adminAuth, requireRole} = require("../controller/adminAuthController");
 
-router.get("/edit/:id", makeupArtist.show)
-router.post("/", makeupArtist.create);
-router.get("/:locationId", makeupArtist.findMakeupArtistByLocation);
-router.get("/admin/:makeupArtistId", makeupArtist.findAppointmentByMakeupArtistId);
-router.put("/:makeupArtistId", makeupArtist.updateMakeupArtist);
-router.delete("/:id", makeupArtist.deleteMakeupArtist)
+// router.get("/edit/:id", makeupArtist.show)
+// router.post("/", makeupArtist.create);
+// router.get("/:locationId", makeupArtist.findMakeupArtistByLocation);
+// router.get("/admin/:makeupArtistId", makeupArtist.findAppointmentByMakeupArtistId);
+// router.put("/:makeupArtistId", makeupArtist.updateMakeupArtist);
+// router.delete("/:id", makeupArtist.deleteMakeupArtist)
 
-// router.get("/edit/:id", adminAuth, requireRole(['PAdmin']), makeupArtist.show)
-// router.post("/", adminAuth, requireRole(['PAdmin']), makeupArtist.create);
-// router.get("/:locationId", adminAuth, requireRole(['PAdmin']), makeupArtist.findMakeupArtistByLocation);
-// router.get("/admin/:makeupArtistId", adminAuth, requireRole(['PAdmin']), makeupArtist.findAppointmentByMakeupArtistId);
-// router.put("/:makeupArtistId", adminAuth, requireRole(['PAdmin']), makeupArtist.updateMakeupArtist);
-// router.delete("/:id", adminAuth, requireRole(['PAdmin']), makeupArtist.deleteMakeupArtist)
+router.get("/edit/:id", adminAuth, requireRole(['PAdmin']), makeupArtist.show)
+router.post("/", adminAuth, requireRole(['PAdmin']), makeupArtist.create);
+router.get("/:locationId", adminAuth, requireRole(['PAdmin']), makeupArtist.findMakeupArtistByLocation);
+router.get("/admin/:makeupArtistId", adminAuth, requireRole(['PAdmin']), makeupArtist.findAppointmentByMakeupArtistId);
+router.put("/:makeupArtistId", adminAuth, requireRole(['PAdmin']), makeupArtist.updateMakeupArtist);
+router.delete("/:id", adminAuth, requireRole(['PAdmin']), makeupArtist.deleteMakeupArtist)
 
 module.exports = router;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,9 @@ import { logout } from "../utilities/users-service"
 export default function NavBar({ setUser }) {
   const token = localStorage.getItem("token")
   const Name =  token ? JSON.parse(window.atob(token.split(".")[1])) : null;
-  const customerName = Name && Name.customer.name ? Name.customer.name : "";
+  const customerName = Name ? Name.customer.name : null;
+  const isAdmin = token && token.user.role === "PAdmin";
+  console.log(`Name: ${Name}`);
 
 const navigate = useNavigate();
 
@@ -23,17 +25,21 @@ const handleAdminLogin = () => {
       {setUser ? (<p>Hello {customerName}</p>): null}
       <nav>
         <ul>
+        {setUser && (
           <li>
             <NavLink to="/maps">Map</NavLink>
           </li>
-
+        )}
+          {setUser && (
           <li>
             <NavLink to="/booking">Appointment Booking</NavLink>
           </li>
-
+          )}
+          {setUser && (
           <li>
             <NavLink to="/history">Upcoming Appointments</NavLink>
           </li>
+          )}
 
           {setUser && (
             <li>
@@ -43,6 +49,11 @@ const handleAdminLogin = () => {
            <li>
            <button onClick={handleAdminLogin}>Admin Login</button>
           </li>
+          {isAdmin && (
+          <li>
+            <NavLink to="/admin">Admin roles</NavLink>
+          </li>
+          )}
         </ul>
       </nav>
     </>
